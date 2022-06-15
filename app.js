7da5f5d4c24972066fed30cfc1660ae1734d5f19
cczerwin762 <cczerwin@udel.edu>
2022-06-14 22:09:42 -0400

    More JS!!

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
-var name = prompt("What is your name?");
-alert("Hello " + name + "! Welcome to Ristorante Italiano home page!");
+
 const backgrounds = ["url(pics/restaurant.jpg)","url(pics/0171477.jpg)","url(pics/Chicken-Parmesan-Barbara-Bakes.jpg)"];
 var backgroundNum = 1;
+
+function hello(){
+  var name = prompt("What is your name?");
+  alert("Hello " + name + "! Welcome to Ristorante Italiano home page!");
+}
 
 function changeBackground(element){
   element.style.backgroundImage = backgrounds[backgroundNum % backgrounds.length];
